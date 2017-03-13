c7deabb90a5df7ed233661f0cf8a1109e3ef5134
Moses Aronov <moses@aronov.me>
2017-03-13 14:55:54 -0400

    created and styled navbar

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app', //The name of the html element 
-    template: '<router-outlet></router-outlet>'//Tells the router where to display the component
+    template: `
+    <navbar></navbar>
+    <router-outlet></router-outlet>`//Tells the router where to display the component
 })
 
 export class AppComponent { }
