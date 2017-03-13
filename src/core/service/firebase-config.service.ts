2dbac28f7164247c536b5e773aed9ff8ad441f5b
Moses Aronov <moses@aronov.me>
2017-03-13 18:47:35 -0400

    Created Firebase-Config Service

---
+++ src/core/service/firebase-config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+
+import * as firebase from 'firebase'
+
+import { FIREBASE_CONFIG } from '../constants/constants'
+
+@Injectable()
+
+export class FirebaseConfigService{
+    constructor(){
+        this.configureApp()
+    }
+    configureApp(){
+        const app = firebase.initializeApp(FIREBASE_CONFIG)
+        console.log(app) //TODO: REMOVE
+    }
+}
