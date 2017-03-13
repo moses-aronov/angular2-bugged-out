b868cac049bc7ad159e3734470daf1cb697b8a6f
Moses Aronov <moses@aronov.me>
2017-03-13 17:41:03 -0400

    Added font-awesome and styled page

--- src/app.component.ts
+++ src/app.component.ts
@@ -8,7 +8,12 @@ import { Component } from '@angular/core';
     <div class="container">
     <router-outlet></router-outlet>
     </div>
-    `//Tells the router where to display the component
+    `,//Tells the router where to display the component
+    styles: [`
+        .container{
+            margin-top: 1.5rem;
+        }
+    `]
 })
 
 export class AppComponent { }
