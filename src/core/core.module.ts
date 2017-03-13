43ee37b20ae61fefadd652eab8794a55c3cf3069
Moses Aronov <moses@aronov.me>
2017-03-13 18:12:10 -0400

    Created and imported core module

---
+++ src/core/core.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core'
+
+@NgModule({
+    imports : [],
+    declarations: [ ],
+    exports: [ ]
+})
+
+export class CoreModule{
+    //Check if the coremodule exits - SINGLETON EXAMPLE
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule ){
+        if(parentModule){
+            throw new Error(
+                "CoreModule exits already. Only import in the root/app module"
+            )
+        }
+    }
+
+    static forRoot(): ModuleWithProviders{
+        return {
+            ngModule : CoreModule,
+            providers: []
+        }
+    }
+}
