cd9526d84b282aab5cc8d7d5213d7ff1b3b1d066
Moses Aronov <moses@aronov.me>
2017-03-13 12:50:42 -0400

    Imported Router Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BugModule } from './bugs/bug.module'
+import { AppRoutingModule } from './app-routing.module'
 
-//Component
+//Component 
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports : [
         BrowserModule,
-        BugModule
+        BugModule,
+        AppRoutingModule
     ],
     declarations : [ AppComponent ],
     bootstrap : [ AppComponent ]
