38cb21125d93d4c2a8fabb90afd99c4f798327c9
Moses Aronov <moses@aronov.me>
2017-03-13 19:04:56 -0400

    Created bug service

---
+++ src/bugs/service/bug.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core'
+
+import { FirebaseConfigService } from '../../core/service/firebase-config.service'
+
+
+@Injectable()
+
+export class BugService{
+    constructor(private fire: FirebaseConfigService){
+
+    }
+}
