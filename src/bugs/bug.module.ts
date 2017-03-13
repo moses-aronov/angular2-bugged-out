40c4b4a3750afa93937333d0f4cf25dbd14f9861
Moses Aronov <moses@aronov.me>
2017-03-13 12:15:45 -0400

    Created and imported shared module

--- src/bugs/bug.module.ts
+++ src/bugs/bug.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
 //Boiler Plate NgModule
 
 @NgModule(
     {
-        imports: [ ],
+        imports: [ 
+            SharedModule
+        ],
         declarations: [ ],
         bootstrap: [ ],
         exports: [ ],
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
+//Boiler Plate NgModule
+
+@NgModule(
+    {
+        imports: [ CommonModule ],
+        declarations: [ ],
+        bootstrap: [ ],
+        exports: [ CommonModule ]
+    }
+)
+
+export class SharedModule {
+
+}
