cc4eb43f6823a1ae342ef63d19c21537bede0f25
Moses Aronov <moses@aronov.me>
2017-03-13 12:46:50 -0400

    Created Router Module

---
+++ src/app-routing.module.ts
@@ -0,0 +1,18 @@
+//Modules
+import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+
+        ])
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {
+
+}
