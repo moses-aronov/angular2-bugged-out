a3e6e491b05216949aee1f74185e1063ac57434d
Moses Aronov <moses@aronov.me>
2017-03-13 12:51:54 -0400

    Added router outlet to component

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,14 +1,9 @@
-//Components 
-//Can have childern, and they could have children
-//They are the building blocks of your application and they are self contained.
-//Very specific and one job.
-//They are in charge of their own html code and styling, everything is self contained, and reusable.
-
+//Component
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app', //The name of the html element 
-    template: '<h1>Skeleton Projects</h1>'//HTML code that will layout the component
+    template: '<router-outlet></router-outlet>'//Tells the router where to display the component
 })
 
 export class AppComponent { }
