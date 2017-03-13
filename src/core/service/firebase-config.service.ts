908bfcf6a09a5c3795fb6466d71796643da50339
Moses Aronov <moses@aronov.me>
2017-03-13 19:02:38 -0400

    Added configureDatabase Function

--- src/core/service/firebase-config.service.ts
+++ src/core/service/firebase-config.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core'
 
 import * as firebase from 'firebase'
+require('firebase/database');
 
 import { FIREBASE_CONFIG } from '../constants/constants'
 
 @Injectable()
 
 export class FirebaseConfigService{
+
+    private database : firebase.database.Database;
+
     constructor(){
         this.configureApp()
+        this.configureDatabase()
     }
+
     configureApp(){
         const app = firebase.initializeApp(FIREBASE_CONFIG)
+    }
+
+    configureDatabase(){
+        //Iniitialize Database
+        this.database = firebase.database()
     }
 }
