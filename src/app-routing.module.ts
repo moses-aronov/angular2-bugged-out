df75c60c801b83455fb821017f59550d214d8822
Moses Aronov <moses@aronov.me>
2017-03-13 12:50:53 -0400

    Created Path to Bug List

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -2,10 +2,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 
+//Component
+import {BugListComponent} from './bugs/bug-list/bug-list.component'
+
 @NgModule({
     imports: [
         RouterModule.forRoot([
-
+            { path: '', component: BugListComponent }
         ])
     ],
     exports: [
