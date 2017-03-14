ddbc12e24d116bf4a041cd2edd1f0975eda7ff1d
Moses Aronov <moses@aronov.me>
2017-03-14 13:55:35 -0400

    Created configureForm group

--- src/bugs/bug-detail/bug-detail.component.ts
+++ src/bugs/bug-detail/bug-detail.component.ts
@@ -1,5 +1,6 @@
 //Modules
-import {Component } from '@angular/core'
+import {Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl} from '@angular/forms'
 
 @Component({
     moduleId: module.id,
@@ -8,6 +9,20 @@ import {Component } from '@angular/core'
     styleUrls: ["bug-detail.component.css"]
 })
 
-export class BugDetailComponent{
+export class BugDetailComponent implements OnInit{
     private modalId = "bugModal";
+    private bugForm: FormGroup;
+
+    ngOnInit(){
+        this.configureform()
+    }
+
+    configureform(){
+        this.bugForm = new FormGroup({
+            title: new FormControl(),
+            status: new FormControl(),
+            severity: new FormControl(),
+            description: new FormControl()
+        })
+    }
 }
