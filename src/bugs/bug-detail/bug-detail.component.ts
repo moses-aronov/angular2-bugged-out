38b28d0a72401aa0ea72ccdae3f9c4f8ff74e26c
Moses Aronov <moses@aronov.me>
2017-03-14 11:40:26 -0400

    Created bugDetail Component

---
+++ src/bugs/bug-detail/bug-detail.component.ts
@@ -0,0 +1,13 @@
+//Modules
+import {Component } from '@angular/core'
+
+@Component({
+    moduleId: module.id,
+    selector: 'bug-detail',
+    templateUrl: 'bug-detail.component.html',
+    styleUrls: ["bug-detail.component.css"]
+})
+
+export class BugDetail{
+    
+}
